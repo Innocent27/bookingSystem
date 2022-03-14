169e4037b5871f469997cc14df258050d7c934bb
Innocent <lindokuhlenkamela@gmail.com>
2022-03-14 14:17:44 +0200

    all routes for user are working(post,get, put, delete)

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
